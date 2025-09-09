fc4b940e37ed84c415c331c077ff2a62c2d24ca9
иг <mcrussia223@gmail.com>
2025-09-09 19:55:37 +0500

    flke8 + black

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 from src.decorators import log
 
+
 @log(filename="mylog.txt")
 def add(x, y):
     return x + y
+
 
 @log()  # Без файла - в консоль
 def divide(a, b):
     return a / b
 
+
 @log(filename="mylog.txt")
 def my_function(x, y):
     return x + y
 
-my_function(1, 2)
 
-# Тестируем
-# add(1, 2)        # Запишет "add ok" в mylog.txt
-# divide(10, 2)    # Выведет "divide ok" в консоль
-# divide(10, 0)    # Выведет ошибку в консоль
+my_function(1, 2)
--- src/decorators.py
+++ src/decorators.py
@@ -1,12 +1,12 @@
 import functools
 
 
-def log(filename=None): # type: ignore
+def log(filename=None):  # type: ignore
     """декоратор для лога выполнения функции"""
 
-    def decorator(func): # type: ignore
+    def decorator(func):  # type: ignore
         @functools.wraps(func)
-        def wrapper(*args, **kwargs): # type: ignore
+        def wrapper(*args, **kwargs):  # type: ignore
             try:
                 result = func(*args, **kwargs)
                 message = f"{func.__name__} ok"
@@ -16,21 +16,21 @@ def log(filename=None): # type: ignore
                 message = f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}"
                 _write_log(message, filename)
                 raise
+
         return wrapper
+
     return decorator
 
 
-def _write_log(message, filename): # type: ignore
+def _write_log(message, filename):  # type: ignore
     """запись лог в файл или консоль"""
     if filename:
-        with open(filename, 'a', encoding='utf-8') as f:
-            f.write(message + '\n')
+        with open(filename, "a", encoding="utf-8") as f:
+            f.write(message + "\n")
     else:
         print(message)
 
 
-
-
-@log(filename="mylog.txt") # type: ignore
-def add(x, y): # type: ignore
+@log(filename="mylog.txt")  # type: ignore
+def add(x, y):  # type: ignore
     return x + y
